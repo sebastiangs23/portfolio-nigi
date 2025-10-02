e1e0823d495e5df062a444818ac02a31a4e8e137
sebastiangs23 <sebastiangs2309@gmail.com>
2025-10-02 20:31:21 +0400

    feat: :sparkles: slider
    upload real pictures, prices and names

---
+++ front/portfolio/src/assets/index.js
@@ -0,0 +1,93 @@
+import caramel_choco from "../assets/slider/caramel-choco-40.jpg";
+import chinese from "../assets/slider/chinese-10.jpg";
+import choco_fondue from "../assets/slider/choco-fondue-40.jpg";
+import chocolate_showpiece from "../assets/slider/chocolate-55.jpg";
+import christmas_tree_pistachio from "../assets/slider/christmas-tree-pistachio-15.jpg";
+import custard_pavlova from "../assets/slider/custard-pavlova-15.jpg";
+import dark_chocolate from "../assets/slider/dark-chocolate-15.jpg";
+import madaleine_xmas from "../assets/slider/madaleine-xmas-tree-100.jpg";
+import mix_fruit from "../assets/slider/mix-fruit-entremet-50.jpg";
+import pistachio from "../assets/slider/pistachio-50.jpg";
+import red_velvet from "../assets/slider/red-velvet-15.jpg";
+import spiders_halloween from "../assets/slider/spiders-halloween-chocolate-5.jpg";
+import strawberry_mousse from "../assets/slider/strawberry-mousse-20.jpg";
+import vanilla from "../assets/slider/vanilla-20.jpg";
+import vanilla_entremet from "../assets/slider/vanilla-entremet-80.jpg";
+
+export const slider_images = [
+  {
+    name: "Dark Chocolate Mousse",
+    image: caramel_choco,
+    price: 40,
+  },
+  {
+    name: "Cake pops Chinese New Year",
+    image: chinese,
+    price: 20,
+  },
+  {
+    name: "Choco Fondue with Matcha green tea",
+    image: choco_fondue,
+    price: 40,
+  },
+  {
+    name: "Chocolate Showpiece",
+    image: chocolate_showpiece,
+    price: 55,
+  },
+  {
+    name: "Christmas tree pistachio tart",
+    image: christmas_tree_pistachio,
+    price: 15,
+  },
+  {
+    name: "Custard pavlova",
+    image: custard_pavlova,
+    price: 15,
+  },
+  {
+    name: "Dark Chocolate mousse with sable",
+    image: dark_chocolate,
+    price: 15,
+  },
+  {
+    name: "Madaleine Xmas Tree Jumbo",
+    image: madaleine_xmas,
+    price: 100,
+  },
+  {
+    name: "Mix Fruit Entremet",
+    image: mix_fruit,
+    price: 50,
+  },
+  {
+    name: "Pistachio Meringue Tart",
+    image: pistachio,
+    price: 50,
+  },
+  {
+    name: "Red Velvet",
+    image: red_velvet,
+    price: 15,
+  },
+  {
+    name: "Spiders Halloween Chocolate",
+    image: spiders_halloween,
+    price: 5,
+  },
+  {
+    name: "Strawberry Mousse Halloween Chocolate",
+    image: strawberry_mousse,
+    price: 20,
+  },
+  {
+    name: "Vanilla Mousse Tart",
+    image: vanilla,
+    price: 20,
+  },
+  {
+    name: "Vanilla Entremet",
+    image: vanilla_entremet,
+    price: 80,
+  },
+];
--- front/portfolio/src/components/Proyectos/Slider.jsx
+++ front/portfolio/src/components/Proyectos/Slider.jsx
@@ -1,4 +1,4 @@
-//Imports
+import { slider_images } from "../../assets";
 import image1 from "./images/9.jpg";
 import image2 from "./images/8.jpg";
 import image3 from "./images/4.jpg";
@@ -7,15 +7,10 @@ import image5 from "./images/9.jpg";
 
 import { FaStar } from "react-icons/fa";
 
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
-// Import required modules
 import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
 
 import { useLanguage } from "../Language/Language";
@@ -24,7 +19,7 @@ import "./Slider2.css";
 
 export default function Slider() {
   const { sentences } = useLanguage();
-    // TODO: MAKE DYNAMIC THE SLIDES AND CHANGE THE COLOR OF THE ARROW
+  // TODO: MAKE DYNAMIC THE SLIDES AND CHANGE THE COLOR OF THE ARROW
   return (
     <section id="tranding">
       <h2 className="section__title">{sentences?._gallery}</h2>
@@ -47,118 +42,32 @@ export default function Slider() {
           navigation
           className="tranding-slider"
         >
-          {/* Slide 1 */}
-          <SwiperSlide className="tranding-slide">
-            <div className="tranding-slide-img">
-              <img src={image1} alt="Tranding" />
-            </div>
-            <div className="tranding-slide-content">
-              <h1 className="food-price">$20</h1>
-              <div className="tranding-slide-content-bottom">
-                <h2 className="food-name">Special Pizza</h2>
-                <h3 className="food-rating">
-                  <span>4.5</span>
-                  <div className="rating">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
+          {slider_images?.map((item, index) => {
+            return (
+              <SwiperSlide className="tranding-slide">
+                <div className="tranding-slide-img">
+                  <img src={item?.image} alt="Tranding" />
+                </div>
+                <div className="tranding-slide-content">
+                  <h1 className="food-price">${item?.price} </h1>
+                  <div className="tranding-slide-content-bottom">
+                    <h2 className="food-name">{item?.name}</h2>
+                    <h3 className="food-rating">
+                      <span>4.5</span>
+                      <div className="rating">
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                        <FaStar />
+                      </div>
+                    </h3>
                   </div>
-                </h3>
-              </div>
-            </div>
-          </SwiperSlide>
+                </div>
+              </SwiperSlide>
+            );
+          })}
 
-          {/* Slide 2 */}
-          <SwiperSlide className="tranding-slide">
-            <div className="tranding-slide-img">
-              <img src={image2} alt="Tranding" />
-            </div>
-            <div className="tranding-slide-content">
-              <h1 className="food-price">$20</h1>
-              <div className="tranding-slide-content-bottom">
-                <h2 className="food-name">Meat Ball</h2>
-                <h3 className="food-rating">
-                  <span>4.5</span>
-                  <div className="rating">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                  </div>
-                </h3>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          {/* Slide 3 */}
-          <SwiperSlide className="tranding-slide">
-            <div className="tranding-slide-img">
-              <img src={image3} alt="Tranding" />
-            </div>
-            <div className="tranding-slide-content">
-              <h1 className="food-price">$40</h1>
-              <div className="tranding-slide-content-bottom">
-                <h2 className="food-name">Burger</h2>
-                <h3 className="food-rating">
-                  <span>4.5</span>
-                  <div className="rating">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                  </div>
-                </h3>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          {/* Slide 4 */}
-          <SwiperSlide className="tranding-slide">
-            <div className="tranding-slide-img">
-              <img src={image4} alt="Tranding" />
-            </div>
-            <div className="tranding-slide-content">
-              <h1 className="food-price">$40</h1>
-              <div className="tranding-slide-content-bottom">
-                <h2 className="food-name">Burger</h2>
-                <h3 className="food-rating">
-                  <span>4.5</span>
-                  <div className="rating">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                  </div>
-                </h3>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          {/* Slide 5 */}
-          <SwiperSlide className="tranding-slide">
-            <div className="tranding-slide-img">
-              <img src={image5} alt="Tranding" />
-            </div>
-            <div className="tranding-slide-content">
-              <h1 className="food-price">$40</h1>
-              <div className="tranding-slide-content-bottom">
-                <h2 className="food-name">Burger</h2>
-                <h3 className="food-rating">
-                  <span>4.5</span>
-                  <div className="rating">
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                    <FaStar />
-                  </div>
-                </h3>
-              </div>
-            </div>
-          </SwiperSlide>
         </Swiper>
       </div>
     </section>
